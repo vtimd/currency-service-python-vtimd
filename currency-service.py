617365172618c6e4b3f2da251124dfa93a272743
Tim Davis <tim.davis.instinct@gmail.com>
2019-09-19 17:58:32 +0000

    Fixed

--- currency-service.py
+++ currency-service.py
@@ -1,10 +1,11 @@
 import requests
 from xml.etree import ElementTree as ET
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
-@app.route("/gbp")
-def gbp():
+@app.route("/gbp/<float:usdvalue>")
+def gbp(usdvalue):
 
     r = requests.get('http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml', stream=True)
     tree = ET.parse(r.raw)
@@ -13,13 +14,16 @@ def gbp():
         for subchild in child:
             for subsubchild in subchild:
                 if subsubchild.attrib['currency'] == 'GBP':
-                    gbprate=subsubchild.attrib['rate']   
+                    gbprate=subsubchild.attrib['rate']  
 
-    print(gbprate)
-    return gbprate  
+    usd = float(usdvalue) * float(gbprate)
+    usdoutput = str(round(usd,2))        
 
-@app.route("/cad")
-def cad():
+#    print(usdoutput)
+    return jsonify (usdoutput), 200
+
+@app.route("/cad/<float:cadvalue>")
+def cad(cadvalue):
 
     r = requests.get('http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml', stream=True)
     tree = ET.parse(r.raw)
@@ -29,8 +33,30 @@ def cad():
             for subsubchild in subchild:
                 if subsubchild.attrib['currency'] == 'CAD':
                     cadrate=subsubchild.attrib['rate']
-    print(cadrate)
-    return cadrate
+
+    cad = float(cadvalue) * float(cadrate)
+    cadoutput = str(round(cad,2))        
+
+#    print(cadoutput)
+    return jsonify (cadoutput), 200 
+
+@app.route("/jpy/<float:jpyvalue>")
+def jpy(jpyvalue):
+
+    r = requests.get('http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml', stream=True)
+    tree = ET.parse(r.raw)
+    root = tree.getroot()
+    for child in root:
+        for subchild in child:
+            for subsubchild in subchild:
+                if subsubchild.attrib['currency'] == 'JPY':
+                    jpyrate=subsubchild.attrib['rate']  
+
+    jpy = float(jpyvalue) * float(jpyrate)
+    jpyoutput = str(round(jpy,2))        
+
+#    print(jpyoutput)
+    return jsonify (jpyoutput), 200 
 
 if __name__ == "__main__":
     app.run(port=5000, host='0.0.0.0')
